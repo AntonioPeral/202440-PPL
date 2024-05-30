c6fcd88cbc78e8f885df2e0728a84c4e27f1fd25
AntonioPeral <146866519+AntonioPeral@users.noreply.github.com>
2024-05-30 09:11:02 -0600

    Add files via upload

---
+++ Sumas de 2 numeros en python.py
@@ -0,0 +1,15 @@
+# Inicio
+
+# Leer num1
+num1 = float(input("Ingresa el primer número: "))
+
+# Leer num2
+num2 = float(input("Ingresa el segundo número: "))
+
+# suma = num1 + num2
+suma = num1 + num2
+
+# Imprimir suma
+print("La suma es:", suma)
+
+# Fin
---
+++ convertir grados C a F.py
@@ -0,0 +1,12 @@
+# Inicio
+
+# Leer celsius
+celsius = float(input("Ingresa la temperatura en grados Celsius: "))
+
+# fahrenheit = (celsius * 9/5) + 32
+fahrenheit = (celsius * 9 / 5) + 32
+
+# Imprimir fahrenheit
+print("La temperatura en grados Fahrenheit es:", fahrenheit)
+
+# Fin
